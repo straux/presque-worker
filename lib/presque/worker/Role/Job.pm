9dd21e3673f89ab6cbf07da60205f7b3d228b096
Stéphane Raux <stephane.raux@linkfluence.net>
2011-06-08 17:21:21 +0200

    add lost job stats; allow infinite retry if job_retries param equals -1

--- lib/presque/worker/Role/Job.pm
+++ lib/presque/worker/Role/Job.pm
@@ -9,15 +9,19 @@ sub _job_failure {
     my ($self, $job, $err) = @_;
 
     push @{$job->{fail}}, $err;
-    my $retries = ($job->{retries_left} || $self->job_retries) - 1;
-    $job->{retries_left} = $retries;
+    my $retries = 1;
+
+    if( $self->job_retries != -1 ) {
+        $retries = ($job->{retries_left} || $self->job_retries) - 1;
+        $job->{retries_left} = $retries;
+    }
     try {
         my %args = ( queue_name => $self->queue_name, payload => $job );
         $args{delayed} = time + $self->delay_on_failure if $self->delay_on_failure;
         if( $retries > 0 ) {
             $self->retry_job( %args );
         } else {
-            $self->retry_job( lost => 1 );
+            $self->retry_job( queue_name => $self->queue_name, lost => 1, payload => {} );
         }
     }
     catch {
