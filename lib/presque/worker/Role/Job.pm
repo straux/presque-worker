04e0b002696d648d13f173ec7eda17523d79b862
Stéphane Raux <stephane.raux@linkfluence.net>
2011-06-09 15:06:25 +0200

    fix stack size for job errors

--- lib/presque/worker/Role/Job.pm
+++ lib/presque/worker/Role/Job.pm
@@ -2,19 +2,32 @@ package presque::worker::Role::Job;
 
 use Try::Tiny;
 use Moose::Role;
-has job_retries    => (is => 'rw', isa => 'Int', default  => 5);
-has delay_on_failure => (is => 'rw', isa => 'Int', default => 0 );
+has job_retries          => (is => 'rw', isa => 'Int', default  => 5);
+has max_error_stack_size => (is => 'rw', isa => 'Int', lazy => 1,
+    default  => sub {
+        my $self = shift;
+        $self->job_retries == -1 ? 20 : $self->job_retries;
+    },
+);
+has delay_on_failure     => (is => 'rw', isa => 'Int', default => 0 );
 
 sub _job_failure {
     my ($self, $job, $err) = @_;
 
     push @{$job->{fail}}, $err;
+    # do not get an error stack too big
+    if( @{$job->{fail}} > $self->max_error_stack_size ) {
+        my @fail = @{$job->{fail}};
+        $job->{fail} = [ @fail[ (-$self->max_error_stack_size)..-1] ];
+    }
+
     my $retries = 1;
 
     if( $self->job_retries != -1 ) {
         $retries = ($job->{retries_left} || $self->job_retries) - 1;
         $job->{retries_left} = $retries;
     }
+
     try {
         my %args = ( queue_name => $self->queue_name, payload => $job );
         $args{delayed} = time + $self->delay_on_failure if $self->delay_on_failure;
